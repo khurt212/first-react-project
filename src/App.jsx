524a45e53e848c5929a6a7027448d45c104471fc
khurt212 <106755123+khurt212@users.noreply.github.com>
2025-06-13 23:54:10 +0800

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,154 @@
+import { useEffect, useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import React from 'react'
+import Search from './Components/Search.jsx'
+import Spinner  from './Components/spinner.jsx'
+import MovieCard from './Components/MovieCard.jsx'
+import { useDebounce } from 'react-use'
+import { getTrendingMovies, updateSearchCount } from './appwrite.js'
+import { configs } from 'eslint-plugin-react-refresh'
+
+
+
+const API_BASE_URL = 'https://api.themoviedb.org/3';
+const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
+console.log('API_KEY:', API_KEY);
+const API_OPTIONS = {
+  method: 'GET',
+  headers: {
+    accept: 'application/json',
+    Authorization: `Bearer ${API_KEY}`
+  }
+};
+
+const App = () => {
+  const [searchTerm, setSearchTerm] = useState('');
+  const [errorMessage, setErrorMessage] = useState('');
+  const [movieList, setMovieList] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [debouncedSearchTerm, setDebouncedSearchTerm] = useState('');
+  const [trendingMovies, setTrendingMovies] = useState([])
+
+  //Debounce the search term to prevent overload on the API 
+  //By waiting for the user to stop typing by 500ms
+  // for installing type in the termila npm then npm i react
+  // useful for any searching
+  useDebounce( ()=> setDebouncedSearchTerm(searchTerm), 600, [searchTerm]);
+
+  const fetchmovies = async (query = '') => {
+    setIsLoading(true);
+    setErrorMessage('');
+    let endpoint = '';
+    if (query) {
+      endpoint = `${API_BASE_URL}/search/movie?query=${encodeURIComponent(query)}`;
+    } else {
+      endpoint = `${API_BASE_URL}/discover/movie?sort_by=popularity.desc`;
+    }
+
+    try {
+      const response = await fetch(endpoint, API_OPTIONS);
+      if (!response.ok) {
+        throw new Error('Failed to fetch movies');
+      }
+      const data = await response.json();
+      if (data.results) {
+        setMovieList(data.results);
+
+        if (query && data.results.length > 0) {
+          try {
+            await updateSearchCount(query, data.results[0]);
+            console.log('Metrics updated for:', query);
+          } catch (err) {
+            console.error('Failed to update metrics:', err);
+          }
+        }
+      } else {
+        setErrorMessage('No movies found.');
+        setMovieList([]);
+      }
+    } catch (error) {
+      console.error(`Error Fetching movies: ${error}`);
+      setErrorMessage('Error Fetching movies. Please try again later.');
+    } finally {
+      setIsLoading(false);
+    }
+  };
+  
+  const loadTrendingMovies = async () => {
+    try{
+      const movies = await getTrendingMovies();
+      
+      setTrendingMovies(movies);
+
+    } catch (error) {
+      console.log(`Error Fetching Trending Movies ${error}`);
+      
+    }
+}
+
+  useEffect(() => {
+    fetchmovies(debouncedSearchTerm);
+  }, [debouncedSearchTerm]);
+
+  useEffect(() => {loadTrendingMovies();}
+  ,[]
+);
+
+  return (
+  <main>
+    <div className='pattern' />
+    
+    
+      <div className='wrapper'>
+
+        <header>
+          <img src="./hero-img.png" alt='Hero Banner' />
+          <h1> <span className='text-gradient'>Find</span> Movies To Watch</h1>
+           <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm}/>
+      
+        </header>
+
+        {trendingMovies.length > 0 && (
+          <section className='trending'>
+          <h2>Trending Movies</h2>
+
+          <ul>{trendingMovies.map((movie, index) =>(
+            <li key={movie.$id}>
+            <p>{index + 1}</p>
+            <img src={movie.poster_url} alt={movie.title} />
+
+            </li>
+
+        ))}</ul>
+          </section>
+        )}
+
+        <section className='all-movies'>
+          <h2>All Movies</h2>
+
+
+          {isLoading ? (<Spinner />) : 
+          errorMessage ? (<p className='text-red-500'>{errorMessage}</p>) : (
+          <ul className='text-center'>
+
+            {movieList.map((movie) => (
+              <MovieCard key={movie.id} movie={movie} />
+
+            ))}
+          </ul>
+        
+            )}
+
+          
+
+        </section>
+
+    </div>
+  </main>
+  )
+    
+  
+};
+export default App
